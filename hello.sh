834ba52291e55fc97cbf1552dab0b18a9691303e
RAPURI HARISH KUMAR <140041270+rapuriharish@users.noreply.github.com>
2024-10-21 13:09:22 +0530

    Update hello.sh
    Hello team here i  find some bugs i fixed that. once look and update .

--- hello.sh
+++ hello.sh
@@ -2,3 +2,13 @@
 echo "Hello World"
 echo "Have a Good day"
 echo "harish"
+
+#!/bin/bash
+echo "Hello World"
+echo "Have a Good day"
+echo "harish"
+
+#!/bin/bash
+echo "Hello World"
+echo "Have a Good day"
+echo "harish"
